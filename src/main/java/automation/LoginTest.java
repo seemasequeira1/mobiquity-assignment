d2c4e22db5a07fcb1589d77174883360ddd2140d
Seema <seemasequeira@gmail.com>
2019-08-25 15:06:01 +0200

    cleaned up

--- src/main/java/automation/LoginTest.java
+++ src/main/java/automation/LoginTest.java
@@ -4,21 +4,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
 
+    ChromeDriver driver;
 
-    public static void loginTest() {
+    @BeforeSuite
+    public void doLoginTest() {
         System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-
+        driver = new ChromeDriver();
         driver.get("http://cafetownsend-angular-rails.herokuapp.com/login");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.cssSelector("#login-form > fieldset > label:nth-child(3) > input")).sendKeys("Luke");
         driver.findElement(By.cssSelector("#login-form > fieldset > label:nth-child(4) > input")).sendKeys("Skywalker");
         driver.findElement(By.cssSelector("#login-form > fieldset > button")).click();
+
+    }
+
+    @AfterSuite
+    public void doLogoutTest(){
+        driver.findElement(By.cssSelector("body > div > header > div > p.main-button")).click();
+        driver.quit();
     }
 }
